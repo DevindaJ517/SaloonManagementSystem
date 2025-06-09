563dc2fc5d6e8b7c4d49662cf1f42688b314dad6
Devinda J <168094003+DevindaJ517@users.noreply.github.com>
2025-06-09 17:16:48 +0530

    front

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Salon!</h1>
+            {/* Add cover image, salon info, featured packages, and reviews preview here */}
+        </div>
+    );
+}
+export default Home;
